11a1b775339bf5ae0a1ef61f16ae14001da5480f
davidecaputo <daca9621@gmail.com>
2023-03-15 00:50:15 +0100

    Add elements to for

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@
             </div>
 */
 
+const boxSlide = document.querySelector('.box-slide');
+
 const immagini = [
     'src="./img/01.webp" alt="spiderman"',
     'src="./img/02.webp" alt="Ratchet & Clank"',
@@ -25,6 +27,12 @@ const immagini = [
 ];
 console.log(immagini);
 
+let slides = '';
 for(i = 0; i < immagini.length; i++){
-    
+    slides += `
+        <div class="slide">
+            <img ${immagini[i]}>
+        </div>
+    `
 }
+console.log(slides);
