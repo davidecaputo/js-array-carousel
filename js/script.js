9ab10cab535b04cadf3a7e3a8259841962b4590f
davidecaputo <daca9621@gmail.com>
2023-03-14 20:46:18 +0100

    Add array

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/*
+            <div class="slide active">
+                <img src="./img/01.webp" alt="spiderman">
+            </div>
+            <div class="slide">
+                <img src="./img/02.webp" alt="Ratchet & Clank">
+            </div>
+            <div class="slide">
+                <img src="./img/03.webp" alt="Fortnite">
+            </div>
+            <div class="slide">
+                <img src="./img/04.webp" alt="Gatto">
+            </div>
+            <div class="slide">
+                <img src="./img/05.webp" alt="Avengers" class="avengers">
+            </div>
+*/
+
+const immagini = [
+    'src="./img/01.webp" alt="spiderman"',
+    'src="./img/02.webp" alt="Ratchet & Clank"',
+    'src="./img/03.webp" alt="Fortnite"',
+    'src="./img/04.webp" alt="Gatto"',
+    'src="./img/05.webp" alt="Avengers" class="avengers"'
+];
